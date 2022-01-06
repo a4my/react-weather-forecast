a8b8a26a699b060e3b8ca4ad66a183bdc2372b93
Alex Fourmy <alexfourmy@gmail.com>
2022-01-06 14:22:14 +0100

    Finished the hourly weather info section

--- components/HourlyWeather.jsx
+++ components/HourlyWeather.jsx
@@ -1,5 +1,6 @@
 import moment from 'moment-timezone'
 import React from 'react'
+import Image from 'next/image'
 
 export default function HourlyWeather({ hourlyWeather, timezone }) {
   console.log(hourlyWeather)
@@ -11,9 +12,22 @@ export default function HourlyWeather({ hourlyWeather, timezone }) {
           hourlyWeather.map((weather, index) => (
             <div className="hourly__box-wrapper" key={weather.dt}>
               <div className="hourly__box">
-                <span className="hourly__time">
-                  {moment.unix(weather.dt).tz(timezone).format('LT')}
+                <span
+                  className={`hourly__time ${
+                    index == 0 ? 'hourly__time--now' : ''
+                  } `}
+                >
+                  {index == 0
+                    ? 'Now'
+                    : moment.unix(weather.dt).tz(timezone).format('LT')}
                 </span>
+                <Image
+                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  alt={weather.weather[0].description}
+                  width="100"
+                  height="100"
+                />
+                <span>{weather.temp.toFixed(0)}&deg;C</span>
               </div>
             </div>
           ))}
